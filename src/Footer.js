1b8031da8a5c8f846e1ae50a07b45395a45da44c
Ashik~ <ashikibrahim7777@gmail.com>
2024-11-30 19:00:47 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span>
+        &copy; {new Date().getFullYear()} RepSetGo. All rights reserved.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
